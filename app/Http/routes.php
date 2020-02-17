b6c351f65564d09f943e4555a6801fee271e0dca
EC2 Default User <ec2-user@ip-172-31-82-40.ec2.internal>
2020-02-17 05:33:06 +0000

    signup

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/','WelcomeController@index');
+
+
+Route::get('signup', 'Auth\AuthController@getRegister')->name('signup.get');
+Route::post('signup','Auth\AuthController@postRegister')->name('signup.post');
+
