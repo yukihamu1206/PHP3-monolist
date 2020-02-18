91300d18048d5d5c02a0750d66e5d1678f576719
EC2 Default User <ec2-user@ip-172-31-82-40.ec2.internal>
2020-02-18 06:46:12 +0000

    item.create

---
+++ app/Http/Controllers/ItemsController.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class ItemsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        // フォームに入力された値を取得する
+        $keyword = request()->keyword;
+        // 配列初期化
+        $items = [];
+        // フォームに入力されていたら
+        if($keyword){
+            $client = new \RakutenRws_Client();
+            $client->setApplicationId(env('RAKUTEN_APPLICATION_ID'));
+            
+            $rws_response = $client->execute('IchibaItemSearch',[
+                'keyword' => $keyword,
+                'imageFlag' => 1,
+                'hints' => 20,
+                ]);
+            
+            // 扱いやすいようにItemとしてインスタンスを作成する（保存はしない）
+            foreach($rws_response->getData()['Items'] as $rws_item){
+                $item = new \App\Item();
+                $item->code = $rws_item['Item']['itemCode'];
+                $item->name = $rws_item['Item']['itemName'];
+                $item->url = $rws_item['Item']['itemUrl'];
+                $item->image_url = str_replace('?_ex=128x128','',$rws_item['Item']['mediumImageUrls'][0]['imageUrl']);
+                $items[] = $item;
+                
+            }
+        }
+        
+        return view('items.create',[
+            'keyword' => $keyword,
+            'items' => $items,
+            ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -13,8 +13,10 @@
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
                     @if(Auth::check())
-                    <li><a href="#"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
-                                アイテムを追加</a></li>
+                    <li><a href="{{ route('items.create') }}">
+                                <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
+                                アイテムを追加
+                            </a></li>
                     
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
